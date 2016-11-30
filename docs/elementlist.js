7f9ac8bf07e3bd0907738aa29253ca9e0c6df677
David Schoenbauer <dschoenbauer@gmail.com>
2016-11-30 13:50:34 -0600

    updated documentation to apigen (#17)

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","DSchoenbauer\\DotNotation\\ArrayDotNotation"],["c","DSchoenbauer\\DotNotation\\Exception\\ExceptionInterface"],["c","DSchoenbauer\\DotNotation\\Exception\\InvalidArgumentException"],["c","DSchoenbauer\\DotNotation\\Exception\\UnexpectedValueException"]];
